3734074f4ab82b8dc46dd581e30032eb549ee16c
dashuddka <98728077+dashuddka@users.noreply.github.com>
2022-04-22 17:22:03 +0700

    Create geometry.c

---
+++ src/geometry/geometry.c
@@ -0,0 +1,35 @@
+#include <ctype.h>
+#include <libgeometry/checkprint.h>
+#include <libgeometry/print.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    int p = 50;
+    int w = 10;
+    char str[p];
+    char re[w];
+
+    while (1) {
+        fgets(str, 50, stdin);
+        str[strlen(str) - 1] = '\0';
+        sscanf(str, "%[^(]%*[\n]", re);
+
+        for (int i = 0; re[i]; i++)
+            re[i] = tolower(re[i]);
+
+        if (strcmp(re, "circle") == 0) {
+            check_circle(str, re);
+
+        } else if (strcmp(re, "triangle") == 0) {
+            check_triangle(str, re);
+
+        } else
+            printf("Invalid input!\n");
+    }
+
+    return 0;
+}
